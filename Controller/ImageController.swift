0ce15b3e89c561fb63591ddb2357036914560a7b
Vlad Deba <vladislavdeba@gmail.com>
2020-06-15 16:53:35 +0300

    Scroll view content inset fixed

--- Controller/ImageController.swift
+++ Controller/ImageController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 protocol Notifiable {
+
 	func movementStarted()
 
 	func colorChanged(to color: UIColor)
@@ -19,6 +20,7 @@ protocol Notifiable {
 class ImageController: UIViewController, Notifiable {
 
 	func movementFinished() {
+		#warning("Check if color is saved")
 		likeButton.setVisible(false)
 	}
 
@@ -37,13 +39,14 @@ class ImageController: UIViewController, Notifiable {
 
 	var likeButton: FlatButton!
 
-	private var scrollView = UIScrollView()
+	var scrollView = UIScrollView()
 
 	private var colorInfoView: ColorInfoView!
 
 	var photoImageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.contentMode = .scaleAspectFit
+		imageView.clipsToBounds = true
 		return imageView
 	}()
 
@@ -64,6 +67,9 @@ class ImageController: UIViewController, Notifiable {
 		scrollView.minimumZoomScale = 1.0
 		scrollView.maximumZoomScale = 8.0
 		scrollView.delaysContentTouches = false
+		scrollView.contentInsetAdjustmentBehavior = .never
+		scrollView.showsVerticalScrollIndicator = false
+		scrollView.showsHorizontalScrollIndicator = false
 
 		view.addSubview(scrollView)
 		scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -71,16 +77,19 @@ class ImageController: UIViewController, Notifiable {
 			scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
 			scrollView.topAnchor.constraint(equalTo: view.topAnchor),
 			scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-			scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
 		])
 
 		scrollView.addSubview(photoImageView)
+		photoImageView.clipsToBounds = true
 		photoImageView.translatesAutoresizingMaskIntoConstraints = false
 		NSLayoutConstraint.activate([
+			photoImageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+			photoImageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+			photoImageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			photoImageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
 			photoImageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-			photoImageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-			photoImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-			photoImageView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor)
+			photoImageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
 		])
 
 		colorInfoView = ColorInfoView()
@@ -120,5 +129,21 @@ extension ImageController: UIScrollViewDelegate {
 	func scrollViewDidZoom(_ scrollView: UIScrollView) {
 		let scale = scrollView.zoomScale
 		pickerView.transform = CGAffineTransform(scaleX: 1/scale, y: 1/scale)
+
+		guard let image = photoImageView.image else { return }
+		let ratioW = photoImageView.frame.width / image.size.width
+		let ratioH = photoImageView.frame.height / image.size.height
+		let ratio = ratioW < ratioH ? ratioW : ratioH
+
+		let newWidth = image.size.width * ratio
+		let newHeight = image.size.height * ratio
+
+		let conditionLeft = newWidth * scale > photoImageView.frame.width
+		let left = 0.5 * (conditionLeft ? newWidth - photoImageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+
+		let conditionTop = newHeight * scale > photoImageView.frame.height
+		let top = 0.5 * (conditionTop ? newHeight - photoImageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+
+		scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
 	}
 }
